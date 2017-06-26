1f94282ca480531c189c46d6c35aa946e38454e1
Erik Cass <erik.cass@gdit.com>
2017-06-25 23:28:56 -0500

    refactor login/user area using bootstrap.

--- server.js
+++ server.js
@@ -1,33 +1,19 @@
 var express = require('express');
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
-var TwitterStrategy = require('passport-twitter').Strategy;
+var twitterStrategy = require('passport-twitter').Strategy;
 var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
 
 var CONTACTS_COLLECTION = "contacts";
 
-// Configure the Facebook strategy for use by Passport.
-//
-// OAuth 2.0-based strategies require a `verify` function which receives the
-// credential (`accessToken`) for accessing the Facebook API on the user's
-// behalf, along with the user's profile.  The function must invoke `cb`
-// with a user object, which will be set at `req.user` in route handlers after
-// authentication.
 passport.use(new FacebookStrategy({
-//    clientID: process.env.CLIENT_ID,
-//    clientSecret: process.env.CLIENT_SECRET,
     clientID: '145826452655768',
     clientSecret: '388560ee4c8d52694ad674617a97e6dd',
     callbackURL: 'https://sheltered-gorge-33033.herokuapp.com/login/facebook/return'
   },
   function(accessToken, refreshToken, profile, cb) {
-    // In this example, the user's Facebook profile is supplied as the user
-    // record.  In a production-quality application, the Facebook profile should
-    // be associated with a user record in the application's database, which
-    // allows for account linking and authentication with other identity
-    // providers.
     return cb(null, profile);
   }));
 
@@ -37,21 +23,13 @@ passport.use(new TwitterStrategy({
     callbackURL: "https://sheltered-gorge-33033.herokuapp.com/login/twitter/return"
   },
   function(token, tokenSecret, profile, cb) {
-    User.findOrCreate({ twitterId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
+    return cb(null, profile);
+  //  User.findOrCreate({ twitterId: profile.id }, function (err, user) {
+  //    return cb(err, user);
+  //  });
   }
 ));
 
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  In a
-// production-quality application, this would typically be as simple as
-// supplying the user ID when serializing, and querying the user record by ID
-// from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Facebook profile is serialized
-// and deserialized.
 passport.serializeUser(function(user, cb) {
   cb(null, user);
 });
@@ -59,7 +37,6 @@ passport.serializeUser(function(user, cb) {
 passport.deserializeUser(function(obj, cb) {
   cb(null, obj);
 });
-
 
 // Create a new Express application.
 var app = express();
