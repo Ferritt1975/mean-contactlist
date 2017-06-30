2288a8f182b0ea05b656a02890927bfcad9083ed
Erik Cass <erik.cass@gdit.com>
2017-06-29 22:02:39 -0500

    add passport-local

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ passport.use(new LocalStrategy({
     passwordField: 'password',
   },
   function(token, tokenSecret, cb) {
-    var User = db.collection(USERS_COLLECTION).find({}).toArray(function(err, docs) {});
+    var User = db.collection(USERS_COLLECTION).find({}).toArray();
     User.findOne({
       'email': token,
     }, function(err, user) {
