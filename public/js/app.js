9e9a5f7ae6a8d38e826dfc18138c59947fe45715
Erik Cass <erik.cass@gdit.com>
2017-07-04 12:42:47 -0500

    add passport-local

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,3 @@
-angular.module('fileUpload', ['ngFileUpload'])
-    .controller('MyCtrl', ['$scope', 'Upload', '$timeout', function ($scope, Upload, $timeout) {
-        $scope.uploadPic = function(file) {
-        file.upload = Upload.upload({
-          url: '/upload',
-          data: {file: file},
-        });
-
-        file.upload.then(function (response) {
-          $timeout(function () {
-                file.result = response.data;
-          });
-        }, function (response) {
-          if (response.status > 0)
-            $scope.errorMsg = response.status + ': ' + response.data;
-        }, function (evt) {
-          // Math.min is to fix IE which reports 200% sometimes
-          file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-        });
-        }
-    }]);
-
 angular.module("contactsApp", ['ngRoute'])
     .config(function($routeProvider) {
         $routeProvider
@@ -99,6 +77,26 @@ angular.module("contactsApp", ['ngRoute'])
                 });
         }
     })
+    .controller('MyCtrl', ['$scope', 'Upload', '$timeout', function ($scope, Upload, $timeout) {
+        $scope.uploadPic = function(file) {
+        file.upload = Upload.upload({
+          url: '/upload',
+          data: {file: file},
+        });
+
+        file.upload.then(function (response) {
+          $timeout(function () {
+                file.result = response.data;
+          });
+        }, function (response) {
+          if (response.status > 0)
+            $scope.errorMsg = response.status + ': ' + response.data;
+        }, function (evt) {
+          // Math.min is to fix IE which reports 200% sometimes
+          file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+        });
+        }
+    }])
     .controller("ListController", function(contacts, $scope) {
         $scope.contacts = contacts.data;
     })
